d190f62b01ce247249ca54be297f358999a8f8df
Alex Sorokine <sorokine@gmail.com>
2014-04-24 23:40:43 -0400

    1st working code for retrieval of edit stats (incomplete)

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var bot = require('nodemw'),
         path: '/w',
         debug: true
     }),
-    params = {
+    myparams = {
         action: 'query',
         prop: 'revisions',
         titles: 'River', 
@@ -12,9 +12,29 @@ var bot = require('nodemw'),
         rvprop: 'timestamp'
     };
 
-client.api.call(params, function(info, next, data) {
-	var pageid = Object.keys(info.pages).shift(); 
-    console.log(info.pages[pageid].revisions.length);
-    console.log(next);
-    console.log(data && data.query.results);
-});
+function getRevisionInfo(params, callback, finalcall) {
+	client.api.call(params, function(info, next, data) {
+		var pageid = Object.keys(data.query.pages).shift(); 
+	    console.log(data); //.query.pages[pageid].revisions);
+	    
+	    callback(data.query.pages[pageid].revisions);
+	    
+	    if (data["query-continue"] && data["query-continue"].revisions) {
+	    	params.rvcontinue = data["query-continue"].revisions.rvcontinue;
+	    	getRevisionInfo(params, callback, finalcall);
+	    } else {
+	    	finallcall && finalcall();
+	    }
+	    
+	});
+}
+
+var rev_count = 0;
+var all_revisions = [];
+getRevisionInfo(myparams, function(revisions) {
+	rev_count += revisions.length;
+	all_revisions = all_revisions.concat(revisions);
+}, function() {
+	console.log(all_revisions);
+	console.log("Revision count " + rev_count);
+} );

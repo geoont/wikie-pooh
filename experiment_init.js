6120a9b634d5804d3b8873540e01ce3d3e804582
Alex Sorokine <sorokine@gmail.com>
2014-11-27 22:30:58 -0500

    servlet and page skeltons for server-based version created

---
+++ experiment_init.js
@@ -0,0 +1,11 @@
+/**
+ * Initializes experiment
+ */
+
+/* process two arguments */
+
+/* stop if database already exists */
+
+/* create database tables */
+
+/* load cats file into the database */
---
+++ experiment_serve.js
@@ -0,0 +1,26 @@
+/**
+ * Server with experiment results
+ */
+
+/* process arguments */
+
+/* open the database */
+
+/* launch the main server */
+
+/* launch sevlets for specific functions */
+
+/*** SERVLETS ***/
+
+/**
+ * Send a list of Wikipedia entries to the browser 
+ */
+
+/**
+ * Receive toggled skip flag and save it in the database 
+ */
+
+/**
+ * Retrieve and parse entries from Wikipedia, insert then into the database
+ */
+
